7c0144660519533006c20724623f0f458536b777
Tyler Donnelly <tylerdonnelly@Tylers-MacBook-Pro.local>
2017-10-03 19:50:01 -0400

    updated with ACF

--- wp-content/themes/donnelly/footer.php
+++ wp-content/themes/donnelly/footer.php
@@ -28,5 +28,10 @@
 
 <?php wp_footer(); ?>
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
+//]]>
+	
+</script>
 </body>
 </html>
